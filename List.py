1d0b17d668b04602db95fbdae4b92a7f5def6148
joynto7 <joyntoghosh7@gmail.com>
2025-09-12 19:12:09 +0600

    Acesseing list in python

---
+++ List.py
@@ -0,0 +1,22 @@
+# a list is a collection which is ordered and changeable.
+# Allows duplicate members.
+
+
+# <<<<<<<<<<<<<<<<<< - - - -- - - - -- - - - ->>>>.>>>>>>>>>>>
+
+#Creating List 
+
+my_list = ["aziz","joy","maria"]
+my_list2 =["sumiaya","ashraful","Wallid"]
+fruits = ["Mango","Orange","Banana","Apple"]
+city =["Dhaka","chaittagong","Rangpur","Manikganj"]
+languages =["Python","C++","java","javascript","c","pHp"]
+Planets = ["Earth","Venus","Jupiter"]
+information = ["Joy",01.23034,"72/1,Banus hydara"]
+
+#accessing a list in python
+  
+print(fruits[0])
+print(my_list2[1])
+print(languages[3])
+print(Planets[2])
