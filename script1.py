c836e8b6aab88a6272668e1ccd149e89ae97b532
joynto7 <joyntoghosh7@gmail.com>
2025-09-15 19:18:26 +0600

    Library --> OS & function-->> Path(abspath)

--- script1.py
+++ script1.py
@@ -3,3 +3,9 @@ def current_directory():
     cwd=os.getcwd()
     print(cwd)
     current_directory
+
+def file_path(filename):
+    path = os.path.abspath(filename)
+    print(path)
+filename="sample.txt"
+file_path(filename)
